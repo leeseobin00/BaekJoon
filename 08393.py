5af01f69990a75ddaf6ddf317d1714b7ba26c54e
서빈이 <bommy00@naver.com>
2023-03-15 20:44:06 +0900

    Solved 2438, 2439, 2739, 8393, 10807
    Solved 2438, 2439, 2739, 8393, 10807

---
+++ 08393.py
@@ -0,0 +1,8 @@
+sum = 0
+
+n = int(input())
+
+for i in range(n+1):
+    sum += i
+
+print(sum)
---
+++ 10807.py
@@ -0,0 +1,6 @@
+n = int(input())
+
+list = list(map(int, input().split()))
+v = int(input())
+
+print(list.count(v))
